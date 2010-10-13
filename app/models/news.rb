edbb0bc6f09b4e92ca7f468a8bacfd48a769d5b3
Jaakko Juutila <jaakko.juutila@gmail.com>
2010-10-13 11:14:54 +0200

    * Added some sections to seeds.rb * Developed the admin layout

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,8 @@
 class News < ActiveRecord::Base
   has_and_belongs_to_many :section
   has_many :comments, :as => :commentable
+  
+  def to_s
+    title
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+miehet = Section.create :name => 'Miehet', :slug => 'miehet'
+Section.create :name => 'Edustus', :slug => 'miehet-edustus', :parent => miehet
+Section.create :name => 'Miehet II', :slug => 'miehet-kakkonen', :parent => miehet
+Section.create :name => 'Miehet III', :slug => 'miehet-kolmonen', :parent => miehet
 
