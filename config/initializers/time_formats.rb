aff03fa9d63790e5f976629734b8399de465594b
Jaakko Juutila <jaakko.juutila@gmail.com>
2010-10-16 18:44:14 +0200

    * Added some icons to. * Changes timezone to Helsinki. * Changed the flash message place inside the content. * Fixed in admin_style.css Content => content.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 DateTime::DATE_FORMATS[:default] = "%Y.%m.%d %H:%M"
-Time::DATE_FORMATS[:default] = "%Y.%m.%d %H:%M"
+Time::DATE_FORMATS[:default] = "%d.%m.%Y %H:%M"
