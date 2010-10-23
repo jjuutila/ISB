307ad25106e043842d0aa88f512f73e76c7e9a40
Jaakko Juutila <jaakko.juutila@gmail.com>
2010-10-23 17:14:07 +0200

    Added leafs scope.

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,7 +3,9 @@ class Section < ActiveRecord::Base
   belongs_to :parent, :class_name => "Section"
   has_many :sections, :foreign_key => "parent_id"
   has_and_belongs_to_many :news
-  has_many :comments, :as => :commentable  
+  has_many :comments, :as => :commentable
+  
+  scope :leafs, :conditions => ["parent_id NOT NULL"]
   
   def self.possible_parents
     self.where(:parent_id => nil).all
