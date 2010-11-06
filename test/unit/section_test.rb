a14cbcfcd887921cfed124a178332a65d1381d1d
Jaakko Juutila <jaakko.juutila@gmail.com>
2010-11-06 22:50:12 +0100

    Added a leafs test.

--- test/unit/section_test.rb
+++ test/unit/section_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "leafs" do
+    assert_equal(Section.leafs.count, 1)
   end
 end
