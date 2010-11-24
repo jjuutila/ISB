f0bf210f7d1c954ffb69f1f6f30ad7e89a5c6ac2
jaskaj <jaakko.juutila@gmail.com>
2010-11-24 18:54:04 +0100

    Added member factory.

---
+++ test/factories/member.rb
@@ -0,0 +1,9 @@
+Factory.define :member do |m|
+  m.first_name Faker::Name.first_name()
+  m.last_name Faker::Name.last_name()
+  m.gender [true, false].sample
+  m.number rand(100)
+  m.all_time_assists rand(1000)
+  m.all_time_goals rand(1000)
+  m.birth_year rand(20) + 1980
+end
