416c6b80da539a98e6a99416a0b21aa8b52e69ed
jaskaj <jaakko.juutila@gmail.com>
2010-11-24 18:54:39 +0100

    Added unit tests for Member.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,33 @@
 # coding: utf-8
 class Member < ActiveRecord::Base
   has_many :seasons, :through => :affairs
+  
+  validates_presence_of :first_name, :last_name, :gender, :all_time_assists, :all_time_goals, :number
+  
+  validates_numericality_of :number, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 99,
+    :message => 'Numero tulee olla väliltä 0-99.'
+    
+  validates_numericality_of :all_time_assists, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 999,
+    :message => 'Syötöt tulee olla väliltä 0-99.'
+    
+  validates_numericality_of :all_time_goals, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 999,
+    :message => 'Maalit tulee olla väliltä 0-99.'
+  
+  validates_numericality_of :birth_year, :greater_than_or_equal_to => 1900,
+    :less_than_or_equal_to => DateTime::now().year(),
+    :message => "Syntymävuosi tulee olla väliltä 1900-.#{DateTime::now().year()}"
+  
+  before_validation :set_defaults
+  
+  
+  def all_time_points
+    2 * all_time_goals + all_time_assists
+  end
+  
+  private
+  
+  def set_defaults
+    all_time_assists = 0 unless all_time_assists
+    all_time_goals = 0 unless all_time_goals
+  end 
 end
---
+++ spec/models/member_spec.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+require File.expand_path("../../spec_helper.rb", __FILE__)
+
+describe Member do
+  context "validations" do
+    it { should have_many(:seasons) }
+    
+    it { should validate_presence_of(:first_name) }
+    
+    it { should validate_presence_of(:last_name) }
+    
+    it { should validate_presence_of(:gender) }
+    
+    it { should validate_presence_of(:all_time_assists) }
+    it { should validate_numericality_of(:all_time_assists).with_message(/Syötöt tulee olla/) }
+    it { should ensure_inclusion_of(:all_time_assists).in_range(0..999).with_message(/Syötöt tulee olla/) }
+    
+    it { should validate_presence_of(:all_time_goals) }
+    it { should validate_numericality_of(:all_time_goals).with_message(/Maalit tulee olla/) }
+    it { should ensure_inclusion_of(:all_time_goals).in_range(0..999).with_message(/Maalit tulee olla/) }
+    
+    it { should validate_presence_of(:number) }
+    it { should validate_numericality_of(:number).with_message(/Numero tulee olla/) }
+    it { should ensure_inclusion_of(:number).in_range(0..99).with_message(/Numero tulee olla/) }
+    
+    it {should ensure_inclusion_of(:birth_year).in_range(1900..DateTime::now().year()).with_message(/Syntymävuosi/) }
+  end
+  
+  context "printing" do
+    it "should return number of all time points" do
+      member = Factory.build(:member, :all_time_assists => 32, :all_time_goals => 3)
+      member.all_time_points.should == 38
+    end
+  end
+end
