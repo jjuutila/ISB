ac1f08aa614bec7ed6f9ac9c795d9ab9f5881ccd
jaskaj <jaakko.juutila@gmail.com>
2010-11-26 19:02:52 +0100

    Added update feature to MemberController.

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -15,10 +15,19 @@ class Admin::MembersController < Admin::BaseController
   def create
     @member = Member.create(params[:member])
     flash[:notice] = "Uusi pelaaja luotu." unless @member.new_record?
-    respond_with(@member, :location => admin_members_url)
+    respond_with @member, :location => admin_members_url
   end
   
   def edit
     respond_with @member = Member.find(params[:id])
+  end
+  
+  def update
+    @member = Member.find(params[:id])
+    
+    if @member.update_attributes(params[:member])
+      flash[:notice] = 'Päivitetty.'
+    end
+    respond_with @member, :location => admin_members_url
   end
 end
--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -43,4 +43,27 @@ describe Admin::MembersController do
       response.should be_success
     end
   end
+  
+  describe "PUT 'update'" do
+    before(:each) do      
+      @member = Factory(:member)
+      @attributes = {:first_name => "John", :last_name => "Smith", :gender => true,
+        :birth_year => 1984, :number => 2}
+    end
+    
+    it "should redirect to index page after successfull update." do
+      put :update, {:member => @attributes, :id => @member.id.to_s }
+      
+      assigns(:member).errors.size.should == 0
+      response.should redirect_to admin_members_path
+    end
+    
+    it "should render edit page if errors in member." do
+      @attributes[:number] = -1
+      put :update, {:member => @attributes, :id => @member.id.to_s }
+        
+      response.should be_success
+      response.should render_template("edit")
+    end
+  end
 end
