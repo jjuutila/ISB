1396f49371d2fbc80e347f175490cecf9d46070e
jaskaj <jaakko.juutila@gmail.com>
2010-11-27 17:09:16 +0100

    Added some tests and refactored SeasonsController.

--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -1,14 +1,10 @@
 # coding: utf-8
 class Admin::SeasonsController < Admin::BaseController
-  # GET /admin/seasons
-  # GET /admin/seasons.xml
+  respond_to :html
+  
   def index
-    @admin_seasons = Season.where(:section_id => selected_section.id)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @admin_seasons }
-    end
+    @seasons = Season.where(:section_id => selected_section.id)
+    respond_with @seasons
   end
 
   # GET /admin/seasons/1
@@ -22,53 +18,37 @@ class Admin::SeasonsController < Admin::BaseController
     end
   end
 
-  # GET /admin/seasons/new
-  # GET /admin/seasons/new.xml
   def new
-    @admin_season = Season.new(:section_id => selected_section.id,
+    @season = Season.new(:section_id => selected_section.id,
       :partitions => [Partition.new(:name => 'Runkosarja', :position => 1)])
-    
-    respond_to do |format|
-      format.html # new.html.haml
-      format.xml  { render :xml => @admin_season }
-    end
+    respond_with @season
   end
 
   # GET /admin/seasons/1/edit
   def edit
-    @admin_season = Season.find(params[:id])
+    @season = Season.find(params[:id])
   end
 
   # POST /admin/seasons
   # POST /admin/seasons.xml
   def create
-    @admin_season = Season.new(params[:season])
-
-    respond_to do |format|
-      if @admin_season.save
-        format.html { redirect_to(admin_seasons_path, :notice => 'Uusi kausi luotu.') }
-        format.xml  { render :xml => @admin_season, :status => :created, :location => admin_seasons_path }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @admin_season.errors, :status => :unprocessable_entity }
-      end
-    end
+    @season = Season.new(params[:season])
+    @season.partitions.first.position = 1
+    @season.save
+    
+    flash[:notice] = "Uusi kausi luotu." unless @season.new_record?
+    respond_with @season, :location => admin_seasons_path
   end
 
   # PUT /admin/seasons/1
   # PUT /admin/seasons/1.xml
   def update
-    @admin_season = Season.find(params[:id])
-
-    respond_to do |format|
-      if @admin_season.update_attributes(params[:season])
-        format.html { redirect_to(admin_seasons_path, :notice => t('.new-instanse-created')) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @admin_season.errors, :status => :unprocessable_entity }
-      end
+    @season = Season.find(params[:id])
+    
+    if @season.update_attributes(params[:season])
+      flash[:notice] = 'Kausi päivitetty.'
     end
+    respond_with @season, :location => admin_seasons_url
   end
 
   # DELETE /admin/seasons/1
---
+++ spec/controllers/seasons_controller_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe Admin::SeasonsController do
+  
+  def create_sections
+    root_level_section = Factory.create(:section)
+    @section = Factory.create(:section, :parent => root_level_section)
+  end
+  
+  before(:each) do
+    create_sections  
+  end
+  
+  describe "GET 'index'" do
+    it "should be successful" do
+      create_sections
+      get 'index'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      assigns(:season).new_record?.should == true
+      response.should be_success
+    end
+  end
+  
+  describe "POST 'create'" do
+    before(:each) do
+      @attributes = {:division => "3. Divisioona", :start_year => 2003, :section_id => @section.id.to_s,
+        :partitions_attributes => {0 => {:name => 'Runkosarja'} } }  
+    end
+    
+    it "should redirect to index page after successfull create." do
+      post :create, :season => @attributes
+      
+      assigns(:season).errors.size.should == 0
+      assigns(:season).new_record?.should == false
+      response.should redirect_to admin_seasons_path
+    end
+  end
+  
+  describe "GET 'edit'" do    
+    before(:each) do
+      @season = Factory(:season, :section => @section)  
+    end
+        
+    it "should be successful" do
+      get 'edit', :id => @season.id.to_s 
+      assigns(:season).id.should == @season.id
+      response.should be_success
+    end
+  end
+  
+  describe "PUT 'update'" do
+    before(:each) do      
+      @season = Factory(:season, :section => @section)
+      @attributes = {:division => "3. Divisioona", :start_year => 2003, :section_id => @section.id.to_s}
+    end
+    
+    it "should redirect to index page after successfull update." do
+      put :update, {:season => @attributes, :id => @season.id.to_s }
+      
+      assigns(:season).errors.size.should == 0
+      response.should redirect_to admin_seasons_path
+    end
+    
+    it "should render edit page if errors in member." do
+      @attributes[:start_year] = 5
+      put :update, {:season => @attributes, :id => @season.id.to_s }
+        
+      assigns(:season).errors.size.should > 0
+      response.should be_success
+      response.should render_template("edit")
+    end
+  end
+end
