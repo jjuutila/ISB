e358979f7054cb25efe78e2e51b0745e58f0904f
jaskaj <jaakko.juutila@gmail.com>
2010-11-29 13:03:37 +0100

    Deleted statistics request test.

--- spec/requests/admin/statistics_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Statistics" do
-  describe "GET /admin/statistics" do
-    it "works! (now write some real specs)" do
-      get admin_statistics_path
-    end
-  end
-end
