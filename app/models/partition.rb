b590b7d64dfa28a6436479e4fb99b5504e4287a8
jaskaj <jaakko.juutila@gmail.com>
2010-11-29 02:18:33 +0100

    Added statistics association to Partition.

--- app/models/partition.rb
+++ app/models/partition.rb
@@ -3,7 +3,8 @@ class Partition < ActiveRecord::Base
   belongs_to :season
   has_many :team_standings
   has_many :matches
+  has_many :statistics
   
-  validates_presence_of :name, :position
+  validates_presence_of :name, :position, :season
   validates_numericality_of :position, :only_integer => true, :greater_than => 0
 end
