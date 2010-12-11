e9d97b2e871f36de9afb82630907b9ffb2239176
jaskaj <jaakko.juutila@gmail.com>
2010-12-11 12:57:05 +0100

    Added unit tests and implementations for TeamStanding.

--- app/models/team_standing.rb
+++ app/models/team_standing.rb
@@ -3,4 +3,24 @@ class TeamStanding < ActiveRecord::Base
   belongs_to :partition
   has_many :home_matches, :class_name => 'Match', :foreign_key => 'home_team_id'
   has_many :visitor_matches, :class_name => 'Match', :foreign_key => 'visitor_team_id'
+  
+  validates_presence_of :name, :partition
+  
+  validates_numericality_of :wins, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :losses, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :overtimes, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :goals_for, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :goals_against, :only_integer => true, :greater_than_or_equal_to => 0
+  
+  def after_initialize
+    self.wins = 0 unless self.wins
+    self.losses = 0 unless self.losses
+    self.overtimes = 0 unless self.overtimes
+    self.goals_for = 0 unless self.goals_for
+    self.goals_against = 0 unless self.goals_against
+  end
+  
+  def points
+    2 * wins + overtimes
+  end
 end
---
+++ spec/models/team_standing_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe TeamStanding do
+  context "validations" do
+    it { should belong_to(:partition) }
+    it { should validate_presence_of(:partition) }
+    
+    it { should have_many(:home_matches) }
+    it { should have_many(:visitor_matches) }
+    
+    it { should validate_presence_of(:name) }
+    
+    it { should validate_numericality_of(:wins) }
+    it { should validate_numericality_of(:losses) }
+    it { should validate_numericality_of(:overtimes) }
+    it { should validate_numericality_of(:goals_for) }
+    it { should validate_numericality_of(:goals_against) }
+  end
+  
+  context "after initialization" do
+    it "numerical fields should be set to 0 if they are not set" do
+      standing = TeamStanding.new
+      standing.wins.should == 0
+      standing.losses.should == 0
+      standing.overtimes.should == 0
+      standing.goals_for.should == 0
+      standing.goals_against.should == 0
+    end
+    
+    it "should not set 0 value to field that is not nil" do
+      standing = TeamStanding.new :wins => 1
+      standing.wins.should == 1
+      standing.losses.should == 0
+      standing.overtimes.should == 0
+      standing.goals_for.should == 0
+      standing.goals_against.should == 0
+    end
+  end
+  
+  context "points" do
+    it "should give two points for a win" do
+      standing = TeamStanding.new :wins => 2
+      standing.points.should == 4
+    end
+    
+    it "should give one point for overtime" do
+      standing = TeamStanding.new :overtimes => 2
+      standing.points.should == 2
+    end
+    
+    it "should give zero points for losses" do
+      standing = TeamStanding.new :losses => 2
+      standing.points.should == 0
+    end
+  end
+end
