151225c2f0255fd9d9a419cecfdcb4175d7250a6
jaskaj <jaakko.juutila@gmail.com>
2011-01-10 20:51:37 +0100

    Implemented index and new.

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,8 +7,8 @@ class Match < ActiveRecord::Base
   validates_associated :visitor_team, :home_team, :partition
   validates_presence_of :visitor_team, :home_team, :partition, :location, :start_time
   
-  validates_numericality_of :home_goals, :only_integer => true, :greater_than_or_equal_to => 0
-  validates_numericality_of :visitor_goals, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :home_goals, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
+  validates_numericality_of :visitor_goals, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
   
   def result
     "#{home_goals}-#{visitor_goals}"
