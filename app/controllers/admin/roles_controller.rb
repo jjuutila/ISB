2027f784610e252f08af4e5427a39cde48dab53f
jaskaj <jaakko.juutila@gmail.com>
2011-01-20 16:43:17 +0100

    Create Affair in Admin::RolesController.

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,13 +1,24 @@
 # coding: utf-8
 class Admin::RolesController < Admin::BaseController
-  respond_to :html
+  respond_to :html, :only => :index
+  respond_to :json, :except => :index
   
   def index
-    @all_members = Member.all
     @season = Season.find(params[:season_id])
+    @unassigned_members = Member.not_in_season @season
     @players = Member.players.in_season @season
     
-    respond_with @all_members
+    respond_with
+  end
+  
+  def create
+    season = Season.find(params[:season_id])
+    member = Member.find(params[:id])
+    
+    affair = Affair.create :season => season, :member => member, :role => params[:role]
+    
+    #puts 'set_role called'
+    respond_with affair
   end
 
 end
--- spec/controllers/admin/roles_controller_spec.rb
+++ spec/controllers/admin/roles_controller_spec.rb
@@ -19,21 +19,40 @@ describe Admin::RolesController do
       Season.stub(:find).with(2) { mock_season }
     end
     
-    it "assigns all members as @all_members" do
-      Member.stub(:all) { [mock_member] }
-      get :index, :season_id => 2
-      assigns(:all_members).should eq([mock_member])
-    end
-    
-    it "assigns all players as @players" do
-      #Member.stub(:players { [mock_member] }
-      get :index, :season_id => 2
-    end
-    
     it "assigns the requested season as @season do" do
       get :index, :season_id => 2
       assigns(:season).should eq(mock_season)
     end
+    
+    it "assigns unassigned members as @unassigned_members" do
+      Member.stub(:not_in_season).with(mock_season) { [mock_member] }
+      get :index, :season_id => 2
+      assigns(:unassigned_members).should eq([mock_member])
+    end
+    
+    it "assigns all players as @players" do
+      Member.stub_chain(:players, :in_season).with(mock_season) { [mock_member] }
+      get :index, :season_id => 2
+      assigns(:players).should eq([mock_member])
+    end
+
+  end
+  
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new affair" do
+        Season.stub(:find).with(2) { mock_season }
+        Member.stub(:find).with(1) { mock_member }
+        Affair.stub(:create).with(:season => mock_season, :member => mock_member, :role => 'player') {
+          mock_affair(:save => true) }
+        
+        post :create, :season_id => 2, :id => 1, :role => 'player'
+        xhr :post, :create, {:season_id => 2, :id => 1, :role => 'player', :format => 'js'}
+        puts response.inspect
+        response.should be_success
+      end
+    end
+
   end
 
 end
