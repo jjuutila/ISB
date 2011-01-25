be54f14458f55401e7949c21c0dc5319c642d6ca
jaskaj <jaakko.juutila@gmail.com>
2011-01-25 19:11:12 +0100

    Implemented Link.to_s.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,8 @@
 class Link < ActiveRecord::Base
   belongs_to :category, :class_name => 'LinkCategory'
   validates_presence_of :category, :name, :url
+  
+  def to_s
+    name
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe Link do
-  it { should belong_to(:category) }
-  it { should validate_presence_of(:category) }
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:url) }
+  describe "validations" do
+    it { should belong_to(:category) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:url) }  
+  end
+  
+  describe "to_s" do
+    it "returns the name" do
+      link = Link.new :name => "FooBar"
+      link.to_s.should == "FooBar"
+    end
+  end  
 end
