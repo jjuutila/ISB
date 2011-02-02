7c2c08ce85d45ac03b574aef5b0104040c60c231
jaskaj <jaakko.juutila@gmail.com>
2011-02-02 13:30:23 +0100

    Refactored test order.

--- spec/controllers/admin/team_standings_controller_spec.rb
+++ spec/controllers/admin/team_standings_controller_spec.rb
@@ -16,6 +16,51 @@ describe Admin::TeamStandingsController do
     Partition.stub(:find).with(@partition.id) { @partition }
   end
   
+  describe "GET new" do
+    it "assigns a new team as @team" do
+      TeamStanding.stub(:new).with(:partition_id => @partition.id) { mock_team_standing }
+      get :new, :season_id => @season.id, :partition_id => @partition.id
+      assigns(:team).should be(mock_team_standing)
+    end
+  end
+  
+  describe "POST create" do
+    describe "with valid params" do
+      before(:each) do
+        TeamStanding.stub(:new).with({'partition' => @partition}) { mock_team_standing(:save => true) }
+      end
+      
+      it "assigns a newly created team as @team" do
+        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
+        assigns(:team).should be(mock_team_standing)
+      end
+      
+      it "redirects to the show partition page" do
+        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
+        response.should redirect_to(admin_season_partition_path(@season, @partition))
+      end
+      
+      it "shows a flash message" do
+        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
+        flash[:notice].should == 'Uusi joukkue luotu.'
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved team as @team" do
+        TeamStanding.stub(:new) { mock_team_standing(:save => false) }
+        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
+        assigns(:team).should be(mock_team_standing)
+      end
+
+      it "re-renders the 'new' template" do
+        TeamStanding.stub(:new) { mock_team_standing(:save => false, :errors => {:anything => "error"}) }
+        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
+        response.should render_template("new")
+      end
+    end
+  end
+  
   describe "GET edit_multiple" do
     it "assigns the requested standings as @standings" do
       TeamStanding.should_receive(:where).with("partition_id = ?", @partition.id) { [mock_team_standing] }
@@ -106,52 +151,6 @@ describe Admin::TeamStandingsController do
       get 'latest'
       response.should redirect_to admin_seasons_path
     end
-  end
-  
-  describe "GET new" do
-    it "assigns a new team as @team" do
-      TeamStanding.stub(:new).with(:partition_id => @partition.id) { mock_team_standing }
-      get :new, :season_id => @season.id, :partition_id => @partition.id
-      assigns(:team).should be(mock_team_standing)
-    end
-  end
-  
-  describe "POST create" do
-    describe "with valid params" do
-      before(:each) do
-        TeamStanding.stub(:new).with({'partition' => @partition}) { mock_team_standing(:save => true) }
-      end
-      
-      it "assigns a newly created team as @team" do
-        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
-        assigns(:team).should be(mock_team_standing)
-      end
-      
-      it "redirects to the show partition page" do
-        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
-        response.should redirect_to(admin_season_partition_path(@season, @partition))
-      end
-      
-      it "shows a flash message" do
-        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
-        flash[:notice].should == 'Uusi joukkue luotu.'
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved team as @team" do
-        TeamStanding.stub(:new) { mock_team_standing(:save => false) }
-        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
-        assigns(:team).should be(mock_team_standing)
-      end
-
-      it "re-renders the 'new' template" do
-        TeamStanding.stub(:new) { mock_team_standing(:save => false, :errors => {:anything => "error"}) }
-        post :create, :season_id => @season.id, :partition_id => @partition.id, :team_standing => {}
-        response.should render_template("new")
-      end
-    end
-
   end
 
 end
