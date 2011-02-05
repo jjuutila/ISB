c06e2fb719aa13f7999e7e4b60e4e3c05ebc90c1
jaskaj <jaakko.juutila@gmail.com>
2011-02-05 14:12:14 +0100

    Refactored Admin::NewsController.

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,86 +1,40 @@
 # coding: utf-8
 
 class Admin::NewsController < Admin::BaseController
-  # GET /admin/news
-  # GET /admin/news.xml
+  respond_to :html
+  
   def index
     @selected_section = selected_section
-    
-    @news = News.in_section(@selected_section)
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @news }
-    end
+    respond_with @news = News.in_section(@selected_section)
   end
 
-  # GET /admin/news/1
-  # GET /admin/news/1.xml
   def show
-    @news = News.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @news }
-    end
+    respond_with @news = News.find(params[:id])
   end
 
-  # GET /admin/news/new
-  # GET /admin/news/new.xml
   def new
-    @news = News.new(:sections => [selected_section])
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @news }
-    end
+    respond_with @news = News.new(:sections => [selected_section])
   end
 
-  # GET /admin/news/1/edit
   def edit
-    @news = News.find(params[:id])
+    respond_with @news = News.find(params[:id])
   end
 
-  # POST /admin/news
-  # POST /admin/news.xml
   def create
     @news = News.new(params[:news])
-
-    respond_to do |format|
-      if @news.save
-        format.html { redirect_to(admin_news_index_path, :notice => 'Uusi uutinen lisätty.') }
-        format.xml  { render :xml => @news, :status => :created, :location => admin_news_index_path }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @news.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash.notice = 'Uusi uutinen lisätty.' if @news.save
+    respond_with @news, :location => admin_news_index_path
   end
 
-  # PUT /admin/news/1
-  # PUT /admin/news/1.xml
   def update
     @news = News.find(params[:id])
-
-    respond_to do |format|
-      if @news.update_attributes(params[:news])
-        format.html { redirect_to(admin_news_index_path, :notice => 'Uutinen päivitetty.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @news.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash.notice = 'Uutinen päivitetty.' if @news.update_attributes(params[:news])
+    respond_with @news, :location => admin_news_index_path
   end
 
-  # DELETE /admin/news/1
-  # DELETE /admin/news/1.xml
   def destroy
     @news = News.find(params[:id])
     @news.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(admin_news_index_url) }
-      format.xml  { head :ok }
-    end
+    respond_with @news, :location => admin_news_index_url
   end
 end
