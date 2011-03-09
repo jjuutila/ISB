0c90bc75ab8a1cf55d6e781cb2ee91a902de1f68
jaskaj <jaakko.juutila@gmail.com>
2011-03-09 15:14:07 +0100

    404 is shown if section is not found in SectionController.

--- app/controllers/section_controller.rb
+++ app/controllers/section_controller.rb
@@ -1,10 +1,18 @@
 # coding: utf-8
 class SectionController < ApplicationController
   respond_to :html
-  before_filter :get_section
+  before_filter :get_section_or_404
   
-  def get_section
+  def get_section_or_404
     @section = Section.find_by_slug params[:section]
+    render_404 if @section.nil?
+  end
+  
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found }
+      format.any  { head :not_found }
+    end
   end
   
   def news
