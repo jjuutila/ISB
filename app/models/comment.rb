0a92a31e3af8b9d26f0b499e047cd6c1f7534a92
jaskaj <jaakko.juutila@gmail.com>
2011-03-09 13:29:39 +0100

    Adjusted comment field lengths.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,5 @@
 # coding: utf-8
-class Comment < ActiveRecord::Base
-  
-  cattr_reader :per_page
-  @@per_page = 10
-  
+class Comment < ActiveRecord::Base  
   default_scope :order => 'created_at DESC'
   
   attr_accessible :title, :content, :author, :email
@@ -13,13 +9,14 @@ class Comment < ActiveRecord::Base
   validates_length_of :title, :in => 2..60, :too_long => "Liian pitkä otsikko.",
     :too_short => "Liian lyhyt otsikko."
     
-  validates_length_of :content, :in => 5..320, :too_long => "Liian pitkä viesti.",
-    :too_short => "Liian lyhyt viesti."
+  validates_length_of :content, :minimum => 5, :too_short => "Liian lyhyt viesti."
     
   validates_length_of :author, :in => 1..40, :too_long => "Liian pitkä nimimerkki.",
     :too_short => "Anna nimimerkki."
+  
+  validates_length_of :email, :in => 6..256, :message => 'Virheellinen sähköpostiosoite.'
     
-  validates_format_of :email, :allow_blank => true, :message => 'Epäkelpo sähköpostiosoite',
+  validates_format_of :email, :allow_blank => true, :message => 'Virheellinen sähköpostiosoite.',
     :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   
   # Just for first state when comments acts as guestbook
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,9 +6,8 @@ describe Comment do
     
     it { should ensure_length_of(:title).is_at_least(2).is_at_most(60).with_short_message(/lyhyt/).with_long_message(/pitkä/) }
     
-    it { should ensure_length_of(:content).is_at_least(5).is_at_most(320).with_short_message(/lyhyt/).with_long_message(/pitkä/) }
+    it { should ensure_length_of(:content).is_at_least(5).with_short_message(/lyhyt/) }
     it { should ensure_length_of(:author).is_at_least(1).is_at_most(40).with_short_message("Anna nimimerkki.").with_long_message(/pitkä/) }
-    
   end
   
   context "to_s" do
