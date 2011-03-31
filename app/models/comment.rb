c8923b7752e75bd159b6844f5b565fe37fe32cc3
jaskaj <jaakko.juutila@gmail.com>
2011-03-31 14:13:58 +0200

    Adjusted Comment validations to meet legacy data.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,15 +6,15 @@ class Comment < ActiveRecord::Base
   
   validates_presence_of :commentable
   
-  validates_length_of :title, :in => 2..60, :too_long => "Liian pitkä otsikko.",
+  validates_length_of :title, :maximum => 60, :too_long => "Liian pitkä otsikko.",
     :too_short => "Liian lyhyt otsikko."
     
-  validates_length_of :content, :minimum => 5, :too_short => "Liian lyhyt viesti."
+  validates_length_of :content, :minimum => 2, :too_short => "Liian lyhyt viesti."
     
   validates_length_of :author, :in => 1..40, :too_long => "Liian pitkä nimimerkki.",
     :too_short => "Anna nimimerkki."
   
-  validates_length_of :email, :in => 6..256, :message => 'Virheellinen sähköpostiosoite.'
+  validates_length_of :email, :maximum => 256, :message => 'Virheellinen sähköpostiosoite.'
     
   validates_format_of :email, :allow_blank => true, :message => 'Virheellinen sähköpostiosoite.',
     :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,9 +4,9 @@ describe Comment do
   context "validations" do
     it { should validate_presence_of(:commentable) }
     
-    it { should ensure_length_of(:title).is_at_least(2).is_at_most(60).with_short_message(/lyhyt/).with_long_message(/pitkä/) }
+    it { should ensure_length_of(:title).is_at_most(60).with_long_message(/pitkä/) }
     
-    it { should ensure_length_of(:content).is_at_least(5).with_short_message(/lyhyt/) }
+    it { should ensure_length_of(:content).is_at_least(2).with_short_message(/lyhyt/) }
     it { should ensure_length_of(:author).is_at_least(1).is_at_most(40).with_short_message("Anna nimimerkki.").with_long_message(/pitkä/) }
   end
   
