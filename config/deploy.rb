22083e1b34d9b2d1a8a08131a445a6eea0d71da5
jaskaj <jaakko.juutila@gmail.com>
2011-05-17 13:46:54 +0200

    Capistrano deployment for Kapsi.

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+
+# Capistrano database.yml task
+require File.expand_path('../deploy/capistrano_database', __FILE__)
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))  # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                                # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby-1.9.2-p180@isb'             # RVM gemset
+set :rvm_type, :user                                    # Deploying to user's home directory
+
+set :application, "isb"
+set :repository,  "git://github.com/jaskaj/ISB.git"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+set :branch, "master"
+
+set :user, "isb"
+
+role :web, "karviainen.kapsi.fi"                    # Your HTTP server, Apache/etc
+role :app, "karviainen.kapsi.fi"                    # This may be the same as your `Web` server
+role :db,  "karviainen.kapsi.fi", :primary => true  # This is where Rails migrations will run
+
+set :deploy_to, "/home/users/isb/isb-web"
+set :use_sudo, false
+
+require 'bundler/capistrano'
+
+namespace :deploy do
+  desc "Custom ISB deployment: stop."
+  task :stop, :roles => :app do
+    invoke_command "cd #{current_path}"
+    invoke_command "thin stop -C config/thin.yml"
+  end
+
+  desc "Custom ISB deployment: start."
+  task :start, :roles => :app do
+    invoke_command "cd #{current_path}"
+    invoke_command "thin start -C config/thin.yml"
+  end
+
+  desc "Custom ISB deployment: restart."
+  task :restart, :roles => :app do
+    invoke_command "cd #{current_path}"
+    invoke_command "thin restart -C config/thin.yml"
+  end
+end
